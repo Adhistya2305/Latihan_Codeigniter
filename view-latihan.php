047a25bb99d4018092a0a184dc0612fa20df5c35
Nafatul Adistianingrum <70364075+Adhistya2305@users.noreply.github.com>
2022-09-11 08:53:35 +0700

    Add files via upload

---
+++ view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Latihan 1 - contoh3</title>
+    </head>
+    <body>
+        Halo kawan... yuk kita belajar web progamming !!! <br>
+        Nilai 1 = <?= $nilai1;  ?><br>
+        Nilai 2 = <?= $nilai2;  ?><br>
+        ini hasil dari pemodelan dengan methode penjumlahan yaitu <br> <?= $nilai1." + ". $nilai2." = ".$hasil; ?>
+    </body>
+</html>
